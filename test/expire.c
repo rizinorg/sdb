4dba2e8ab33d4b54e3079544fee3511016cc9623
Riccardo Schirone <sirmy15@gmail.com>
2020-12-19 01:20:19 +0100

    Fix compilation on windows

--- test/expire.c
+++ test/expire.c
@@ -1,14 +1,23 @@
 #include <sdb.h>
+#if defined _WIN32
+#include <synchapi.h>
+#define sleep Sleep
+#else
 #include <assert.h>
+#endif
 
 int main() {
 	Sdb *s = sdb_new0 ();
 	sdb_set (s, "key", "val", 0);
 	sdb_expire_set (s, "key", 3, 0);
 	sleep (1);
-	assert (sdb_const_get (s, "key", 0) != NULL);
+	if (sdb_const_get (s, "key", 0) == NULL) {
+		exit(1);
+	}
 	sleep (3);
-	assert (sdb_const_get (s, "key", 0) == NULL);
+	if (sdb_const_get (s, "key", 0) == NULL) {
+		exit(1);
+	}
 	sdb_free (s);
 	return 0;
 }
